3fb869ca064a1b15f02057e4e472b40ee0d413ac
gordonpm <gordonm0815@gmail.com>
2020-06-06 18:20:20 -0700

    1) Separated out recipes and shopping list and made them viewable when you click links on navigation bar

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg'),
     new Recipe('A Test Recipe', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg')
   ];
 
