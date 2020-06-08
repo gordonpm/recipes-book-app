81a1b79798c452d8c4c7c65102edb23a4b71ba3d
gordonpm <gordonm0815@gmail.com>
2020-06-08 13:02:38 -0700

    1) Added clicking on recipe to get recipe detail 2) Can add new ingredient on shopping list page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg'),
-    new Recipe('A Test Recipe', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg')
+    new Recipe('Test Recipe 1', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg'),
+    new Recipe('Test Recipe 2', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
