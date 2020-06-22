2a8e3727ac3487ddba5abf301e9aa313423e5c0f
gordonpm <gordonm0815@gmail.com>
2020-06-22 16:54:57 -0700

    Added Shopping List Service and refactored components to use this service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,37 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe 1', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg'),
-    new Recipe('Test Recipe 2', 'This is a test', 'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg')
+    new Recipe(
+      'Puri Bhaji', 
+      'Tasty snack', 
+      'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg',
+      [
+        new Ingredient('Puri', 5),
+        new Ingredient('Potatoes', 4)
+      ]),
+    new Recipe(
+      'Bhel Puri', 
+      'Appetizer', 
+      'https://storage.needpix.com/rsynced_images/recipes-1152341_1280.jpg',
+      [
+        new Ingredient('Bhel', 2),
+        new Ingredient('Sev', 1)
+      ])
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
